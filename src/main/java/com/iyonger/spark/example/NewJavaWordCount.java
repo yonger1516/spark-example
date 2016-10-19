9d66640c99b7a5c18b1cd2073b386db419e8723c
yonger1516 <sum2000@live.cn>
2016-10-19 11:19:04 +0800

    add example

--- src/main/java/com/iyonger/spark/example/NewJavaWordCount.java
+++ src/main/java/com/iyonger/spark/example/NewJavaWordCount.java
@@ -1,11 +1,15 @@
 package com.iyonger.spark.example;
 
+import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.function.FlatMapFunction;
+import org.apache.spark.api.java.function.Function2;
+import org.apache.spark.api.java.function.PairFunction;
 import org.apache.spark.sql.SparkSession;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import scala.Char;
+import scala.Tuple2;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -35,9 +39,29 @@ public class NewJavaWordCount {
                 logger.info("line after replacement:"+line);
                 return Arrays.asList(SPACE.split(line)).iterator();
             }
-
-
         });
+
+        JavaPairRDD<String, Integer> ones = words.mapToPair(
+                new PairFunction<String, String, Integer>() {
+                    @Override
+                    public Tuple2<String, Integer> call(String s) {
+                        return new Tuple2<>(s, 1);
+                    }
+                });
+
+        JavaPairRDD<String, Integer> counts = ones.reduceByKey(
+                new Function2<Integer, Integer, Integer>() {
+                    @Override
+                    public Integer call(Integer i1, Integer i2) {
+                        return i1 + i2;
+                    }
+                });
+
+        List<Tuple2<String, Integer>> output = counts.collect();
+        for (Tuple2<?,?> tuple : output) {
+            System.out.println(tuple._1() + ": " + tuple._2());
+        }
+        spark.stop();
 
     }
 
@@ -50,15 +74,6 @@ public class NewJavaWordCount {
 
         return line;
     }
-
-    public static List<Character> getSplitChars(String word) {
-        List<Character> chars = new ArrayList<Character>();
-        for (char c : word.toCharArray()) {
-            if (c < 65 || (c > 90 && c < 97) || c > 122) {
-                chars.add(c);
-            }
-        }
-        return chars;
-    }
+    
 
 }
