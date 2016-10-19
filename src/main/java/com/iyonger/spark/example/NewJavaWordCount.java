e68cde32714cfc3b3c6b9e78eda0a8e7303e06d2
yonger1516 <sum2000@live.cn>
2016-10-19 13:03:12 +0800

    bug fix

--- src/main/java/com/iyonger/spark/example/NewJavaWordCount.java
+++ src/main/java/com/iyonger/spark/example/NewJavaWordCount.java
@@ -68,7 +68,7 @@ public class NewJavaWordCount {
     public static String replaceNonLetter(String line) {
         for (char c : line.toCharArray()) {
             if (c < 65 || (c > 90 && c < 97) || c > 122) {
-                line.replace(c, ' ');
+                line=line.replace(c, ' ');
             }
         }
 
